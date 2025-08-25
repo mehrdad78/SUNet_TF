381b984659467f772f938a1314cbc7eeee1e2f13
mehrdad78 <mehrdadch148@gmail.com>
2025-08-25 18:13:39 +0330

    czcv

--- train.py
+++ train.py
@@ -473,7 +473,7 @@ for epoch in range(start_epoch, OPT['EPOCHS'] + 1):
         # weights & losses
         weights = make_weights_from_numpy(target, k=K_RINGS, stroke_w=STROKE_W, ring_w=RING_W)
         # Log once per N steps to avoid spam (adjust N as you like)
-        N = 20
+        N = 10
         global_step = (epoch - 1) * len(train_loader) + i
         if global_step % N == 0:
             log_weight_debug(writer, 'train/weights', global_step, target, weights)
